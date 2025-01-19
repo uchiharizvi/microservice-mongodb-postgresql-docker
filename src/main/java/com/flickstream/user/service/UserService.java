aef9d58416c35bc3188b59a274830fbd9f3e89af
Kavish Rizvi <rizvikavish205@gmail.com>
2025-01-19 16:51:12 +0530

    Added Code

--- src/main/java/com/flickstream/user/service/UserService.java
+++ src/main/java/com/flickstream/user/service/UserService.java
@@ -6,6 +6,6 @@ import java.util.Optional;
 
 public interface UserService {
     User registerUser(User user);
-//    Optional<User> authenticate(String email, String password);
+    Optional<User> authenticate(String email, String password);
     Optional<User> getUser(String email);
 }
--- src/main/java/com/flickstream/user/service/UserServiceImpl.java
+++ src/main/java/com/flickstream/user/service/UserServiceImpl.java
@@ -21,21 +21,21 @@ public class UserServiceImpl implements UserService{
         return userRepository.save(user);
     }
 
-//    @Override
-//    public Optional<User> authenticate(String email, String password) {
-//        //check cache
+    @Override
+    public Optional<User> authenticate(String email, String password) {
+        //check cache
 //        User cachedUser = redisCache.getUserFromCache(email);
 //        if (cachedUser != null && cachedUser.getPassword().equals(password)) {
 //            return Optional.of(cachedUser);
 //        }
-//        //fetch from DB
-//        User user = userRepository.findByEmail(email);
-//        if(user!=null && user.getPassword().equals(password)) {
+        //fetch from DB
+        User user = userRepository.findByEmail(email);
+        if(user!=null && user.getPassword().equals(password)) {
 //            redisCache.cacheUser(user);
-//            return Optional.of(user);
-//        }
-//        return Optional.empty();
-//    }
+            return Optional.of(user);
+        }
+        return Optional.empty();
+    }
 
     @Override
     public Optional<User> getUser(String email) {
